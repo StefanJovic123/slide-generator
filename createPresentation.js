d021f9c1a25c06381549ab8b283e0ee64909bf40
Stefan Jovic <stefan.jovic.up@gmail.com>
2024-09-04 12:13:31 +0200

    feat: add some more generations

--- createPresentation.js
+++ createPresentation.js
@@ -9,7 +9,7 @@ const {
 const path = require("path");
 const fs = require("fs").promises;
 
-const templatePath = path.join(__dirname, "pptx-templates", "RootTemplate.pptx");
+const templatePath = path.join(__dirname, "pptx-templates", "RootTemplateWithImages.pptx");
 const outputDir = path.join(__dirname, "output");
 const outputPath = path.join(outputDir, "presentation.pptx");
 
@@ -20,15 +20,24 @@ async function createPresentation() {
   // Ensure the output directory exists
   await fs.mkdir(outputDir, { recursive: true });
 
+  const data1 = {
+    body: [
+      { label: 'item test r1', values: ['Header 1', 'header 2', 'header 3', 'header 4', 'header 4'] },
+      { label: 'item test r1', values: ['test1', 10, 16, 12, 11] },
+      { label: 'item test r2', values: ['test2', 12, 18, 15, 12] },
+      { label: 'item test r3', values: ['test3', 14, 12, 11, 14] },
+    ],
+  };
+
+
   // Initialize Automizer with preferences
   const automizer = new Automizer({
-    templateDir: `pptx-templates`,
-    outputDir: `output`,
-    useCreationIds: false,
-    autoImportSlideMasters: true,
+    templateDir: `${__dirname}/pptx-templates`,
+    outputDir: `${__dirname}/output`,
+    mediaDir: `${__dirname}/images`,
     removeExistingSlides: true,
-    cleanup: false,
-    compression: 0,
+    useCreationIds: true,
+    cleanup: true,
   });
 
   // Log the template directory to verify the path
@@ -38,6 +47,8 @@ async function createPresentation() {
   // Load the root template and additional templates
   let pres = automizer
     .loadRoot(templatePath)
+    .loadMedia([`test.png`])
+    .load(`RootTemplateWithImages.pptx`, 'base')
     .load(`SlideWithTables.pptx`, 'tables')
     .load(`EmptySlide.pptx`, 'empty')
     .load(`SlideWithCharts.pptx`, 'charts')
@@ -45,13 +56,56 @@ async function createPresentation() {
     .load(`TextReplace.pptx`)
     .load(`SlideWithImages.pptx`, 'images')
 
-  const data1 = {
-    body: [
-      { label: 'item test r1', values: ['test1', 10, 16, 12, 11] },
-      { label: 'item test r2', values: ['test2', 12, 18, 15, 12] },
-      { label: 'item test r3', values: ['test3', 14, 12, 11, 14] },
-    ],
-  };
+
+    await pres
+    .addSlide('charts', 2, (slide) => {
+      slide.modifyElement('ColumnChart', [
+        modify.setChartData({
+          series: [
+            {label: 'series 1'},
+            {label: 'series 2'},
+            {label: 'series 3'},
+          ],
+          categories: [
+            {label: 'cat 2-1', values: [50, 50, 20]},
+            {label: 'cat 2-2', values: [14, 50, 20]},
+            {label: 'cat 2-3', values: [15, 50, 20]},
+            {label: 'cat 2-4', values: [26, 50, 20]}
+          ]
+        })
+      ]);
+    })
+
+    await pres
+    .addSlide('tables', 3, async (slide) => {
+      const elements =await slide.getAllElements();
+      console.log(elements)
+      slide.removeElement('TableWithEmptyCells')
+      slide.removeElement('TableWithFormattedCells')
+      // slide.removeElement('Titel 2')
+
+      slide.addElement('tables', 3, 'Titel 2', [
+        modify.setText('Table title'),
+        ModifyShapeHelper.setPosition({
+          x: CmToDxa((25.4 - 15) / 2), // Center horizontally (slide width - element width) / 2
+          y: CmToDxa(4), // Position 4 cm from the top
+          w: CmToDxa(15), // Element width
+          h: CmToDxa(2), // Element height
+        })
+      ]);
+
+      slide.modifyElement('EmptyTable', [
+        modify.setTable(data1),
+        ModifyShapeHelper.setPosition({
+          x: CmToDxa((25.4 - 15) / 2), // Center horizontally (slide width - element width) / 2
+          y: CmToDxa(6), // Position 4 cm from the top
+          w: CmToDxa(15), // Element width
+          h: CmToDxa(5), // Element height
+        }),
+        // modify.dump
+      ]);
+    })
+  
 
   // const data2 = {
   //   body: [
@@ -176,23 +230,23 @@ async function createPresentation() {
   //   ],
   // };
 
-  pres.addSlide('images', 1);
-  pres.addSlide('images', 2);
+  // pres.addSlide('images', 1);
+  // pres.addSlide('images', 2);
 
-  await pres.addSlide('tables', 3, (slide) => {
-    slide.modifyElement('TableWithEmptyCells', [
-      modify.setTable(data1),
-      // modify.dump
-    ]);
-  })
+  // await pres.addSlide('tables', 3, (slide) => {
+  //   slide.modifyElement('TableWithEmptyCells', [
+  //     modify.setTable(data1),
+  //     // modify.dump
+  //   ]);
+  // })
 
-  pres.addSlide('charts', 1);
+  // pres.addSlide('charts', 1);
 
-  await pres
-    .addSlide('empty', 1, (slide) => {
-      slide.addElement('charts', 2, 'PieChart');
-      slide.addElement('charts', 1, 'StackedBars');
-    })
+  // await pres
+  //   .addSlide('empty', 1, (slide) => {
+  //     slide.addElement('charts', 2, 'PieChart');
+  //     slide.addElement('charts', 1, 'StackedBars');
+  //   })
 
   // await pres.addSlide('tables', 3, (slide) => {
   //   slide.modifyElement('TableWithEmptyCells', [
@@ -202,116 +256,116 @@ async function createPresentation() {
   // })
 
   // Add a slide from the template and use getAllTextElementIds inside the callback
-  await pres.addSlide('twoTextElementPres.pptx', 1, async (slide) => {
-    // Use the getAllTextElementIds method to get all text element IDs in the slide
-    const elementIds = await slide.getAllTextElementIds();
+  // await pres.addSlide('twoTextElementPres.pptx', 1, async (slide) => {
+  //   // Use the getAllTextElementIds method to get all text element IDs in the slide
+  //   const elementIds = await slide.getAllTextElementIds();
 
-    // Loop through the element IDs and modify the text
-    for (const elementId of elementIds) {
-      slide.modifyElement(
-        elementId,
-        modify.replaceText(
-          [
-            {
-              replace: 'placeholder',
-              by: {
-                text: 'New Text',
-              },
-            },
-            {
-              replace: 'placeholder2',
-              by: {
-                text: 'New Text 2',
-              },
-            },
-          ],
-          {
-            openingTag: '{',
-            closingTag: '}',
-          },
-        ),
-      );
-    }
-  });
+  //   // Loop through the element IDs and modify the text
+  //   for (const elementId of elementIds) {
+  //     slide.modifyElement(
+  //       elementId,
+  //       modify.replaceText(
+  //         [
+  //           {
+  //             replace: 'placeholder',
+  //             by: {
+  //               text: 'New Text',
+  //             },
+  //           },
+  //           {
+  //             replace: 'placeholder2',
+  //             by: {
+  //               text: 'New Text 2',
+  //             },
+  //           },
+  //         ],
+  //         {
+  //           openingTag: '{',
+  //           closingTag: '}',
+  //         },
+  //       ),
+  //     );
+  //   }
+  // });
 
-  await pres
-    .addSlide('TextReplace.pptx', 1, (slide) => {
-      slide.modifyElement('setText', modify.setText('Test'));
+  // await pres
+  //   .addSlide('TextReplace.pptx', 1, (slide) => {
+  //     slide.modifyElement('setText', modify.setText('Test'));
 
-      slide.modifyElement(
-        'replaceText',
-        modify.replaceText(
-          [
-            {
-              replace: 'replace',
-              by: {
-                text: 'Apples',
-              },
-            },
-            {
-              replace: 'by',
-              by: {
-                text: 'Bananas',
-              },
-            },
-            {
-              replace: 'replacement',
-              by: [
-                {
-                  text: 'Really!',
-                  style: {
-                    size: 10000,
-                    color: {
-                      type: 'srgbClr',
-                      value: 'ccaa4f',
-                    },
-                  },
-                },
-                {
-                  text: 'Fine!',
-                  style: {
-                    size: 10000,
-                    color: {
-                      type: 'schemeClr',
-                      value: 'accent2',
-                    },
-                  },
-                },
-              ],
-            },
-          ],
-          {
-            openingTag: '{{',
-            closingTag: '}}',
-          },
-        ),
-      );
-    })
-    .addSlide('TextReplace.pptx', 2, (slide) => {
-      slide.modifyElement(
-        'replaceTextBullet1',
-        modify.replaceText(
-          [
-            {
-              replace: 'bullet1',
-              by: {
-                text: bulletPoints,
-              },
-            },
-            {
-              replace: 'bullet2',
-              by: {
-                text: bulletPoints,
-              },
-            },
-          ],
-          {
-            openingTag: '{{',
-            closingTag: '}}',
-          },
-        ),
-      );
-    })
+  //     slide.modifyElement(
+  //       'replaceText',
+  //       modify.replaceText(
+  //         [
+  //           {
+  //             replace: 'replace',
+  //             by: {
+  //               text: 'Apples',
+  //             },
+  //           },
+  //           {
+  //             replace: 'by',
+  //             by: {
+  //               text: 'Bananas',
+  //             },
+  //           },
+  //           {
+  //             replace: 'replacement',
+  //             by: [
+  //               {
+  //                 text: 'Really!',
+  //                 style: {
+  //                   size: 10000,
+  //                   color: {
+  //                     type: 'srgbClr',
+  //                     value: 'ccaa4f',
+  //                   },
+  //                 },
+  //               },
+  //               {
+  //                 text: 'Fine!',
+  //                 style: {
+  //                   size: 10000,
+  //                   color: {
+  //                     type: 'schemeClr',
+  //                     value: 'accent2',
+  //                   },
+  //                 },
+  //               },
+  //             ],
+  //           },
+  //         ],
+  //         {
+  //           openingTag: '{{',
+  //           closingTag: '}}',
+  //         },
+  //       ),
+  //     );
+  //   })
+  //   .addSlide('TextReplace.pptx', 2, (slide) => {
+  //     slide.modifyElement(
+  //       'replaceTextBullet1',
+  //       modify.replaceText(
+  //         [
+  //           {
+  //             replace: 'bullet1',
+  //             by: {
+  //               text: bulletPoints,
+  //             },
+  //           },
+  //           {
+  //             replace: 'bullet2',
+  //             by: {
+  //               text: bulletPoints,
+  //             },
+  //           },
+  //         ],
+  //         {
+  //           openingTag: '{{',
+  //           closingTag: '}}',
+  //         },
+  //       ),
+  //     );
+  //   })
 
 
   // Save the presentation
